5a2b8a409287ac7d3177dd88f97da617ca70c148
Shivam Soni <36334280+phoenix30s@users.noreply.github.com>
2020-04-02 12:42:22 +0530

    fix : 1 fixed the name convention in xml.       2 Change in string concatenation method.

--- app/src/main/java/com/example/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -18,48 +18,48 @@ public class MainActivity extends AppCompatActivity {
 
     public void pointers3a(View view) {
         scoreboard_a = scoreboard_a + 3;
-        TextView tx = (TextView) findViewById(R.id.tvsba);
-        tx.setText("" + scoreboard_a);
+        TextView tx = findViewById(R.id.tv_sb_a);
+        tx.setText(String.valueOf(scoreboard_a));
     }
 
     public void pointers2a(View view) {
         scoreboard_a = scoreboard_a + 2;
-        TextView tx = (TextView) findViewById(R.id.tvsba);
-        tx.setText("" + scoreboard_a);
+        TextView tx = findViewById(R.id.tv_sb_a);
+        tx.setText(String.valueOf(scoreboard_a));
+
     }
 
-    public void freethrowa(View view) {
-        scoreboard_a = scoreboard_a + 1;
-        TextView tx = (TextView) findViewById(R.id.tvsba);
-        tx.setText("" + scoreboard_a);
+    public void freeThrow_a(View view) {
+        scoreboard_a++;
+        TextView tx = findViewById(R.id.tv_sb_a);
+        tx.setText(String.valueOf(scoreboard_a));
     }
 
     public void pointers3b(View view) {
         scoreboard_b = scoreboard_b + 3;
-        TextView tx = (TextView) findViewById(R.id.scoreboardB);
-        tx.setText("" + scoreboard_b);
+        TextView tx = findViewById(R.id.tv_sb_b);
+        tx.setText(String.valueOf(scoreboard_b));
     }
-
     public void pointers2b(View view) {
         scoreboard_b = scoreboard_b + 2;
-        TextView tx = (TextView) findViewById(R.id.scoreboardB);
-        tx.setText("" + scoreboard_b);
+        TextView tx = findViewById(R.id.tv_sb_b);
+        tx.setText(String.valueOf(scoreboard_b));
     }
 
-    public void freethrowb(View view) {
+    public void freeThrow_b(View view) {
         scoreboard_b = scoreboard_b + 1;
-        TextView tx = (TextView) findViewById(R.id.scoreboardB);
-        tx.setText("" + scoreboard_b);
+        TextView tx = findViewById(R.id.tv_sb_b);
+        tx.setText(String.valueOf(scoreboard_b));
     }
 
     public void reset(View view) {
         scoreboard_a=0;
         scoreboard_b =0;
 
-        TextView txa = (TextView) findViewById(R.id.tvsba);
-        txa.setText("" + scoreboard_a);
-        TextView txb = (TextView) findViewById(R.id.scoreboardB);
-        txb.setText("" + scoreboard_b);
+        TextView tx_a = findViewById(R.id.tv_sb_a);
+        tx_a.setText(String.valueOf(scoreboard_a));
+        TextView tx_b = findViewById(R.id.tv_sb_b);
+        tx_b.setText(String.valueOf(scoreboard_b));
 
     }
 
